2b750d1c0e0cfaa464569f7251e387bedb5cfcd2
Sasindu <sasindudilhara@2018.com>
2023-08-29 23:57:12 +0530

    Recat native setup for web

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+
+const {height} = Dimensions.get('screen');
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.center}>
+      <Text>......</Text>
+        <Text>This is React Native Web!!!</Text>
+      </View>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    height,
+  },
+  center: {
+    flex: 1,
+    justifyContent:  'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
